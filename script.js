31c9843c75368d768ec2994c03fb6d8996980f36
Thyago Lima <129916981+thyagolima17771@users.noreply.github.com>
2023-04-25 13:56:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const column = document.querySelector(".column");
+const columns = document.querySelector("input[type = file]")
+column.addEventListener("change", function() {
+    if (this.column && this.column[0]) {
+        columns.innerHTML = this.column[0].name;
+    }
+});
+
+document.addEventListener("dragstart", (e) => {
+  e.target.classList.add("dragging");
+});
+
+document.addEventListener("dragend", (e) => {
+  e.target.classList.remove("dragging");
+});
+
+columns.forEach((item) => {
+  item.addEventListener("dragover", (e) => {
+    const dragging = document.querySelector(".dragging");
+    const applyAfter = getNewPosition(item, e.clientY);
+
+    if (applyAfter) {
+      applyAfter.insertAdjacentElement("afterend", dragging);
+    } else {
+      item.prepend(dragging);
+    }
+  });
+});
+
+function getNewPosition(column, posY) {
+  const cards = column.querySelectorAll(".item:not(.dragging)");
+  let result;
+
+  for (let refer_card of cards) {
+    const box = refer_card.getBoundingClientRect();
+    const boxCenterY = box.y + box.height / 2;
+
+    if (posY >= boxCenterY) result = refer_card;
+  }
+
+  return result;
+}
